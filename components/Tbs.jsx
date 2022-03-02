cd3a3d537d241653f12e7bab88ad7e2eea6d01c9
sphade <sphade010@gmail.com>
2022-03-02 09:36:31 +0100

    fixed 2 bug

--- components/Tbs.jsx
+++ components/Tbs.jsx
@@ -15,8 +15,14 @@ const Tbs = () => {
     "https://fakestoreapi.com/products/categories",
     fetcher
   );
+
   if (!data) {
-    return  <div className="w-full flex items-center justify-center">  <ClipLoader  size={150} /></div>  
+    return (
+      <div className="w-full flex items-center justify-center">
+        {" "}
+        <ClipLoader size={150} />
+      </div>
+    );
   }
   if (error) {
     return <div>error occured</div>;
@@ -25,10 +31,10 @@ const Tbs = () => {
     <Tabs selectedTabClassName="border outline-red-100 bg-red-500 text-xs rounded-full text-white">
       <TabList className="my-6 mx-5  flex flex-row space-x-3 items-center ">
         {data.map((name) => (
-          <Tab >{name}</Tab>
+          <Tab>{name}</Tab>
         ))}
       </TabList>
-      {data.map((name) => (
+      {data.slice(0, 2).map((name) => (
         <TabPanel>
           <Card3 cat={name} />
         </TabPanel>
